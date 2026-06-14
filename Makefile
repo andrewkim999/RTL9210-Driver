25e6d27703da04ecb77981874f18d72a444f1cc8
andrewkim999 <andrewkim270@gmail.com>
2026-06-14 15:18:20 +0900

    changed filename

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 KDIR := /home/andrewkim999/test/wsl2-kernel
 
-obj-m += sys/rtl9210.o
+obj-m += src/rtl9210.o
 
 all:
 	make -C $(KDIR) M=$(PWD) modules
--- bind_rtl9210.sh
+++ bind_rtl9210.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-sudo rmmod driver 2>/dev/null
-sudo insmod driver.ko
+sudo rmmod rtl9210 2>/dev/null
+sudo insmod src/rtl9210.ko
+echo "2-1:1.0" | sudo tee /sys/bus/usb/drivers/usb-storage/unbind
+echo "2-1:1.0" | sudo tee /sys/bus/usb/drivers/rtl9210/bind
 dmesg
