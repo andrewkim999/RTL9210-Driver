0c4dec9400af5ef1a1a8d0c26b62ecb9ceed49a3
andrewkim999 <andrewkim270@gmail.com>
2026-08-08 15:45:44 -0500

    added make and make clean to script

--- bind_rtl9210.sh
+++ bind_rtl9210.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+make clean
 sudo rmmod rtl9210 2>/dev/null
+make
 sudo insmod src/rtl9210.ko
 echo "2-1:1.0" | sudo tee /sys/bus/usb/drivers/usb-storage/unbind
 echo "2-1:1.0" | sudo tee /sys/bus/usb/drivers/rtl9210/bind
