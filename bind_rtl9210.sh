8dd0ca06a6fd8cd2d02a792f2cc8543b349ae75b
andrewkim999 <andrewkim270@gmail.com>
2026-08-11 03:31:17 -0500

    guarantees mounting new driver

--- bind_rtl9210.sh
+++ bind_rtl9210.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-sudo rmmod rtl9210 2>/dev/null
+sudo umount /dev/sde1 2>/dev/null
+sudo rmmod rtl9210
+echo "exit code: $?"
+lsmod | grep rtl9210
+
+make clean && make
 sudo insmod src/rtl9210.ko
+
 echo "2-1:1.0" | sudo tee /sys/bus/usb/drivers/usb-storage/unbind
 echo "2-1:1.0" | sudo tee /sys/bus/usb/drivers/rtl9210/bind
 dmesg
